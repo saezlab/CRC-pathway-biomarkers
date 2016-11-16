c6523d772dd60e5b2f1994c3e6b742fc22b4f636
eduati <eduati@ebi.ac.uk>
2016-11-16 17:18:45 +0100

    analysis of optimisation results

---
+++ R/getAllOptResults.R
@@ -0,0 +1,193 @@
+#  Copyright (c) 2016 - EMBL-EBI
+#
+#  File author(s): Federica Eduati (federica.eduati@gmail.com)
+#
+#  Distributed under the GPLv3 License.
+#  See accompanying file LICENSE.txt or copy at
+#      http://www.gnu.org/licenses/gpl-3.0.html
+#
+#  Website: https://github.com/saezlab/CRC-pathway-biomarkers
+# --------------------------------------------------------
+
+#' @title retrieve bootstrap optimisation results for all cell lines
+#'
+#' @description
+#' \code{getAllOptResults_bootstrap} return the optimised parameters and some performance scores
+#'
+#' @details
+#' This function reads the file from the optimisation run on the cluster and returns optimised
+#' parameters and scores for each run and each cell line (e.g. bootstrap was run ~150 times for each cell line)
+#'
+#' @param resultsFolder Path of the folder with the results of the optimisation (form the cluster)
+#' @param namesSignals Name of the readouts (to compute scores)
+#' @param optimisedPars Parameters estimated: can be "k", "tau", "n" or a combination of those
+#' @param N Optional number of runs to consider (to have the same number of runs for all cell lines)
+#' 
+#' @return This fuction returns a list one element for each cell line with "scores" and "parameters".
+#'    Scores are organised as data frame with one run for each row and one metric for each column. Metrics are:
+#'    - MSE: mean squared error
+#'    - SS_tot: total sum of squares
+#'    - SS_res: residual sum of squares
+#'    - COD: coefficient of determination (or R^2)
+#'    - corrCoeff: Pearson correlation
+#'    - corrPval: p-value for Pearson correlation
+#'    Parameters are organised as matrix with parameters on the columns and runs on the rows
+
+getAllOptResults_bootstrap <- function(resultsFolder, namesSignals, optimisedPars, N="all"){
+  currentFolder<-getwd()
+  
+  setwd(resultsFolder)
+  
+  AllFiles<-list.files(path = ".", pattern = NULL, all.files = FALSE,
+                       full.names = FALSE, recursive = FALSE,
+                       ignore.case = FALSE, include.dirs = FALSE, no.. = FALSE)
+  
+  currentPath<-resultsFolder
+  
+  allByCellLine<-lapply(AllFiles, function(cellLineFolder){
+    setwd(paste(currentPath, cellLineFolder, sep=""))
+    AllRunsFile<-list.files(path = ".", pattern = NULL, all.files = FALSE,
+                            full.names = FALSE, recursive = FALSE,
+                            ignore.case = FALSE, include.dirs = FALSE, no.. = FALSE)
+    AllRunsFile<-AllRunsFile[grep(".RData", AllRunsFile)]
+    AllRunsFile<-setdiff(AllRunsFile, "eSSR_report.RData")
+    
+    #####
+    #### compute null hypothesis for COD for each cell line (randomising data)
+    #   load(AllRunsFile[[1]])
+    #   ix_signals<-which(cnolist$namesSignals %in% namesSignals)
+    #   
+    #   y_meas<-as.vector(cnolist$valueSignals[[2]][,ix_signals])
+    #   y_meas<-y_meas[!is.na(y_meas)]
+    #   
+    #   # compute total sum of squares (SS_tot)
+    #   y_meas_mean<-mean(y_meas)
+    #   SS_tot<-sum((y_meas-y_meas_mean)^2)
+    #   
+    #   # compute SS_res and COD for randomised predictions
+    #   COD_random<-sapply(seq(1:10000), function(x){
+    #     
+    #     # generate random predictions from permutated data
+    #     y_pred<-sample(y_meas, replace = F)
+    #     
+    #     # compute residual sum of squares
+    #     SS_res<-sum((y_meas-y_pred)^2)
+    #     
+    #     # compute the coefficient of determination (COD or R2)
+    #     COD=1-SS_res/SS_tot
+    #     
+    #     # return(COD)
+    #     return(COD)
+    #   })
+    #   #########
+    # # })
+    
+    allRunsScores<-do.call(rbind, lapply(AllRunsFile, function(runFile){
+      
+      res.df<-tryCatch({
+        load(runFile)
+        ix_signals<-which(cnolist$namesSignals %in% namesSignals)
+        
+        
+        if (any(simulatedData[[1]][,ix_signals] != 0.5) | any(cnolist$valueSignals[[1]][,ix_signals] != 0.5)){cat("something wrong t0 should always be 0.5")}
+        
+        y_meas<-as.vector(cnolist$valueSignals[[2]][,ix_signals])
+        y_pred<-as.vector(simulatedData[[2]][,ix_signals])
+        # remove NAs
+        ix_na<-(is.na(y_meas) | is.na(y_pred))
+        y_meas<-y_meas[ix_na==F]
+        y_pred<-y_pred[ix_na==F]
+        
+        # compute MSE
+        MSE<-mean((y_meas-y_pred)^2)
+        
+        # compute total sum of squares (SS_tot)
+        y_meas_mean<-mean(y_meas)
+        SS_tot<-sum((y_meas-y_meas_mean)^2)
+        
+        # compute residual sum of squares
+        SS_res<-sum((y_meas-y_pred)^2)
+        
+        # compute the coefficient of determination (COD or R2)
+        COD=1-SS_res/SS_tot
+        # COD_pval<-length(which(COD_random>-COD))/length(COD_random) # uncomment this to have the p value
+        
+        # compute r (from correlation)
+        corrCoeff<-cor.test(y_meas, y_pred)$estimate
+        corrPval<-cor.test(y_meas, y_pred)$p.value
+        
+        # res.df<-data.frame(MSE=MSE, SS_tot=SS_tot, SS_res=SS_res, COD=COD, COD_pval=COD_pval, corrCoeff=corrCoeff, corrPval=corrPval)  # this also shows pvalues for COD
+        res.df<-data.frame(MSE=MSE, SS_tot=SS_tot, SS_res=SS_res, COD=COD, corrCoeff=corrCoeff, corrPval=corrPval)
+        
+        if (sum(is.na(simulatedData[[2]][,ix_signals]))>0){
+          cat("\n---------\nThere are NAs in the simulation, check\n---------\n")
+        }
+        return(res.df)
+      },
+      error=function(cond){
+        message(cond)
+        # cat("pippo")
+        cat("\n not able to load", runFile)
+        
+        return(NULL)
+      })
+      
+      return(res.df)
+      
+    }))
+    rownames(allRunsScores)<-seq(1, nrow(allRunsScores))
+    
+    
+    allRunsParameters<-do.call(rbind, lapply(AllRunsFile, function(runFile){
+      
+      parsValues<-tryCatch({
+        load(runFile)
+        if (length(optimisedPars)==3 & all(optimisedPars %in% c("tau", "k", "n"))){
+          parsValues<-opt_pars$parValues
+          names(parsValues)<-opt_pars$parNames
+        }else if (length(optimisedPars)==2 & all(optimisedPars %in% c("tau", "k"))){
+          parsValues<-opt_pars$parValues[c(opt_pars$index_k, opt_pars$index_tau)]  
+          names(parsValues)<-opt_pars$parNames[c(opt_pars$index_k, opt_pars$index_tau)]
+        }else if (length(optimisedPars)==1 & all(optimisedPars %in% c("k"))){
+          parsValues<-opt_pars$parValues[opt_pars$index_k]
+          names(parsValues)<-opt_pars$parNames[opt_pars$index_k]
+        }else{
+          cat("Error: please specify which parameters has been optimised")
+        }
+        return(parsValues)
+      },
+      error=function(cond){
+        message(cond)
+        # cat("pippo")
+        cat("\n not able to load", runFile)
+        
+        return(NULL)
+      })
+      #       
+      return(parsValues)
+    }))
+    
+    # if N is provided consider only first N runs of the bootstrap
+    if (is.numeric(N) & N < nrow(allRunsScores)){
+      allRunsScores<-allRunsScores[1:N,]
+      allRunsParameters<-allRunsParameters[1:N,]
+    }
+    
+    return(list(scores=allRunsScores, parameters=allRunsParameters))
+    
+  })
+  
+  # retrieve cell line from fileName
+  cellLinesNames<-sapply(AllFiles, function(x) {
+    strsplit(x, split="_")[[1]][1]
+  })
+  cellLinesNames<-sapply(cellLinesNames, function(x) {
+    strsplit(x, split="-")[[1]][2]
+  })
+  
+  names(allByCellLine)<-cellLinesNames
+  
+  setwd(currentFolder)
+  
+  return(allByCellLine)
+}
---
+++ analysis/analyse_optimisation_results.R
@@ -0,0 +1,68 @@
+# packages
+# library()
+
+# R functions
+source("/Users/eduati/CRC-pathway-biomarkers/R/getAllOptResults.R")
+source("/Users/eduati/CRC-pathway-biomarkers/R/getAllROC.R")
+
+
+
+# data
+load("/Users/eduati/CRC-pathway-biomarkers/data/base/metadata.RData")
+load("/Users/eduati/CRC-pathway-biomarkers/data/base/namesSignals.RData")
+
+
+
+######
+# L1 regularisation on tau
+
+######
+# L1 regularisation k
+
+######
+# bootstrap
+resultsFolder = "/Users/eduati/CRC-pathway-biomarkers/data/processed/optimisation/bootstrap/"
+optimisedPars=c("k", "tau")
+allOptResults<-getAllOptResults_bootstrap(resultsFolder=resultsFolder, namesSignals = namesSignals, optimisedPars=c("k", "tau"), N=150)
+cellLinesNames<-names(allOptResults)
+
+# take a look at the scores
+allScores<-do.call(rbind, lapply(cellLinesNames, function(x){
+  tmp_data<-allOptResults[[x]]$scores
+  stat_means<-apply(tmp_data,2,mean)
+  stat_sd<-apply(tmp_data,2,sd)
+  data.frame(cellName=x, value=stat_means, sd=stat_sd, label=colnames(tmp_data))
+}))
+allScores_plot<-subset(allScores, label %in% c("MSE", "COD", "corrCoeff"))
+
+# plot all scores
+ggplot(allScores_plot, aes(x=cellName, y=value)) + 
+  geom_bar(position=position_dodge(), stat="identity") +
+  geom_errorbar(aes(ymin=value-sd, ymax=value+sd),
+                width=.2,                    
+                position=position_dodge(.9)) + theme_bw() +
+  theme(axis.text.x=element_text(angle=45, hjust=1)) + facet_grid(label ~ ., scales="free_y")
+ggsave("/Users/eduati/CRC-pathway-biomarkers/figs/allScoresBarplot.pdf", width=6, height=4)
+
+# plot total sum of squares (SS_tot) vs residual sum of squares (SS_res)
+SS_tot_all<-subset(allScores, label=="SS_tot")
+SS_res_all<-subset(allScores, label=="SS_res")
+pdf("/Users/eduati/CRC-pathway-biomarkers/figs/SStot_vs_SSres.pdf",width=6,height=6)
+plot(SS_tot_all$value, SS_res_all$value, xlim=c(0,30), ylim=c(0,30), pch=16, xlab="total sum of squares (SS_tot)", ylab="residual sum of squares (SS_res)")
+text(SS_tot_all$value, SS_res_all$value, labels = SS_tot_all$cellName, pos = 1)
+abline(a=0, b=1)
+dev.off()
+
+# plot only MSE (for Figure 2)
+MSE_barplot<-subset(allScores, label=="MSE")
+ggplot(MSE_barplot, aes(x=cellName, y=value)) +
+  geom_bar(position=position_dodge(), stat="identity") +
+  geom_errorbar(aes(ymin=value-sd, ymax=value+sd),
+                width=.2,
+                position=position_dodge(.9)) + theme_bw() +
+  theme(axis.text.x=element_text(angle=45, hjust=1)) +
+  labs(x="cell line", y="MSE (mean squared error)")
+ggsave("/Users/eduati/CRC-pathway-biomarkers/figs/MSE.pdf", width=8, height=4)
+
+# compute ROC curves (can be slow depending on th_vec)
+resROCallCellLines<-getAllROC(resultsFolder, namesSignals, posClass=c("up", "down", "noc"), th_vec=seq(0, 0.5, 0.001))
